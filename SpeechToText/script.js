0688b81eb4c46c40176cf4011ef51e1de6159f83
Faiz Shaikh <77228339+sk-faiz@users.noreply.github.com>
2021-06-30 12:35:58 +0530

    Add files via upload

---
+++ SpeechToText/script.js
@@ -0,0 +1,49 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+let saveHandle
+
+var Textbox = $("#textarea");
+var instructions = $("#instructions");
+
+var Content = "";
+
+recognition.continuous = true;
+
+recognition.onresult = function (event) {
+  var current = event.resultIndex;
+
+  var transcript = event.results[current][0].transcript;
+
+  Content += transcript;
+  Textbox.val(Content);
+};
+
+$("#start").on("click", function (e) {
+  if ($(this).text() == "Stop Recording") {
+    $(this).html("Start Recording");
+    $("#instructions").html("");
+    recognition.stop();
+  } else {
+    $(this).html("Stop Recording");
+    $("#instructions").html("Voice Recognition is on");
+    if (Content.length) {
+      Content += " ";
+    }
+    recognition.start();
+  }
+});
+
+$("#clear").click(function () {
+  Textbox.val("");
+  $("#load").html("Load File")
+  Content = ""
+  $("#start").html("Start Recording")
+});
+
+Textbox.on("input", function () {
+  Content = $(this).val();
+});
+
+
+
